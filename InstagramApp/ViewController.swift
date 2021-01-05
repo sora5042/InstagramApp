8ffcca7d3ecdd6a62e77669e316938a38b308a67
ooya sora <sora-110507@ezweb.ne.jp>
2021-01-05 12:01:29 +0900

    Initial Commit

---
+++ InstagramApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by 大谷空 on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
